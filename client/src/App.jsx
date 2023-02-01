e3d89d5195cc6a0c01bd9e4f00586efbffced1f9
Wilson Chan <wilsonchan54@gmail.com>
2023-02-01 00:23:30 -0500

    Created routes in client App.jsx for Home and CreatePost pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { logo } from './assets';
 
-function App() {
-  const [count, setCount] = useState(0)
+import { Home, CreatePost } from './pages';
 
+
+
+const App = () => {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <BrowserRouter>
+      <header className='w-full flex justify-between items-center bg-slate-500
+      sm:px-8 px-4 py-4 border-b border-b-[orange]'>
+        <Link to='/'>
+          <img src={logo} alt="nice logo :)" className='w-28 object-contain' />
+          <h1 className='font-bold'>How?</h1>
+        </Link>
+        <Link to='/create-post' className='font-inter font-medium 
+        bg-[crimson] text-white px-4 py-2 rounded-md'>
+          Create!
+        </Link>
+      </header>
+        
+      <main className='sm:p-8 px-4 py-8 w-full 
+      bg-[cadetblue] min-h-[calc(100vh-73px)]'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/create-post' element={<CreatePost />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
   )
 }
 
