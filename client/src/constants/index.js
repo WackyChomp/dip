7064fd9a0e54b446391fea880b40b1c4431cda34
Wilson Chan <wilsonchan54@gmail.com>
2023-01-31 23:43:39 -0500

    Added prompts in constants index.js / installed tailwindcss / added tailwindcss code snippet for index.css

---
+++ client/src/constants/index.js
@@ -0,0 +1,6 @@
+export const funSamplePrompts = [
+    'Mona Lisa full body portrait in 80s outfit in NYC oil painting',
+    'Wheres Waldo inside Roman coliseum surrounded by thousands of people in real life',
+    'Power Rangers vs Teen Titans side by side with the same Teen Titans cartoon art style',
+    'Humanoid lobster spitting water from its mouth fighting seagulls and pterodactyls in 80s art style',
+];
