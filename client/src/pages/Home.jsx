9678e13be64b9f4c2f74b87ae5148a9915ec0cb6
Wilson Chan <wilsonchan54@gmail.com>
2023-02-03 00:23:38 -0500

    Created homepage with placeholder content and styling / updated README.md

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,49 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+
+import { Card, FormField, Loader } from '../components';
 
 const Home = () => {
+
+  const [loading, setLoading] = useState(false);
+  const [allPosts, setAllPosts] = useState(null);
+
+  const [searchText, setSearchText] = useState('asdf');
+
+
   return (
-    <div>Home</div>
+    <section className='max-w-7xl mx-auto'>
+      <div>
+        <h1 className='font-extrabold text-[gold] text-[32]'>The Community</h1>
+        <p className='mt-2 text-[#666e75] text-[15px] max-w[500px]'>
+          Browse through the gallery of AI generated images
+        </p>
+      </div>
+
+      <div className='mt-16'>
+        <FormField />
+      </div>
+
+      <div className="mt-10">
+        {loading ? (
+          <div className="flex justify-center items-center">
+            <Loader />
+          </div>
+          ) : (
+            <>
+              {searchText && (
+                <h2 className='font-medium text-[red] text-xl mb-3'>
+                  Showing Results:
+                  <span className='text-[greenyellow]'>
+                    {searchText}
+                  </span>
+                </h2>
+
+              )}
+            </>
+          )
+        }
+      </div>
+    </section>
   )
 }
 
